ee57190a216c6b2e90a96d410006e0078d54ff1d
dmitrimuhhin <dmitri.muhhin@gmail.com>
2022-11-05 09:40:55 +0200

    [refactor] - better package naming

---
+++ src/main/java/ee/secretagency/endofthegame/homework1/Calculator.java
@@ -0,0 +1,29 @@
+package ee.secretagency.endofthegame.homework1;
+
+
+import org.springframework.web.bind.annotation.*;
+
+import java.io.Serializable;
+
+@RestController
+@RequestMapping("/calculator")
+public class Calculator {
+
+    // http://localhost:8080/calculator/add?a=5&b=15
+    @GetMapping("/add")
+    public int sum(@RequestParam(value = "a", defaultValue = "0") int a,
+                   @RequestParam(value = "b", defaultValue = "0") int b) {
+        return a+b;
+    }
+
+    @GetMapping("/div/{a}/{b}")
+    public  <T> Serializable div(@PathVariable float a,
+                                 @PathVariable float b){
+        if(b==0){
+            return "Can't divide with zero";
+        } else {
+            return a/b;
+        }
+    }
+
+}
