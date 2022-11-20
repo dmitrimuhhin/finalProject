7d17baa9a88f30572680943938cedd2f804bbc0c
dmitrimuhhin <dmitri.muhhin@gmail.com>
2022-11-20 11:59:45 +0200

    [refactor] - using one place for storing urls

--- frontend/src/app/components/not-found/not-found.component.ts
+++ frontend/src/app/components/not-found/not-found.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {homeUrl} from "../../models/urls";
 
 @Component({
   selector: 'app-not-found',
@@ -7,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NotFoundComponent implements OnInit {
 
+  home = homeUrl
   constructor() { }
 
   ngOnInit(): void {
