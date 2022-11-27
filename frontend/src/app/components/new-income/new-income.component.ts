862fbd93e8b0349a301c5178dab86c49ae2905db
dmitrimuhhin <dmitri.muhhin@gmail.com>
2022-11-27 11:46:21 +0200

    [add/change] - added form for creating new income, using router to change current page after sending new income to backend

--- frontend/src/app/components/new-income/new-income.component.ts
+++ frontend/src/app/components/new-income/new-income.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup} from "@angular/forms";
+import {IncomeService} from "../../services/income-service/income.service";
+import {Income} from "../../models/income";
+import {Router} from "@angular/router";
+import {incomeListUrl} from "../../models/urls";
 
 @Component({
   selector: 'app-new-income',
@@ -25,12 +30,40 @@ export class NewIncomeComponent implements OnInit {
     'GBP'
   ]
 
-  constructor() { }
+  incomeForm = new FormGroup({
+    category: new FormControl<string>('SALARY'),
+    currency: new FormControl<string>('EUR'),
+    amount: new FormControl<number>(0),
+    person: new FormControl<string>(''),
+    timestamp: new FormControl<Date>(new Date())
+  })
+
+  constructor(private incomeService: IncomeService,
+              private router: Router) { }
 
   ngOnInit(): void {
   }
 
   onSubmitData() {
     console.log("sending data to backend")
+    console.log("form value: " + JSON.stringify(this.incomeForm.value, null, 2))
+    const income = this.createIncomeBasedOnFormValues()
+    this.incomeService.createNewIncome(income)
+      .subscribe(
+        () => this.router.navigate([incomeListUrl])
+      )
+  }
+
+  private createIncomeBasedOnFormValues(): Income {
+    return {
+      id: null,
+      category: this.incomeForm.value.category || 'SALARY',
+      currency: this.incomeForm.value.currency || 'EUR',
+      amount: this.incomeForm.value.amount || 0,
+      person: this.incomeForm.value.person || '',
+      timestamp: this.incomeForm.value.timestamp?.toISOString() || new Date().toDateString(),
+      creationTimestamp: null,
+      updateTimestamp: null
+    }
   }
 }
